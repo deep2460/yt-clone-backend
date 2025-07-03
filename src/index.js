9d6af680fe5db5616560e59fc450073771e7e9a6
deepak dhaked <deepakdhaked21@gmail.com>
2025-07-04 00:39:20 +0530

    create utils and create server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv" //used through ecperimental feature check in package.json 
 dotenv.config({path: './.env'})
 import mongodbConnect from "./db/index.js";
+import { app } from "./app.js";
 
-import express from "express"
-const app = express();
 
 mongodbConnect()
+.then(() =>{
+  app.listen(process.env.PORT || 8000, ()=>{console.log(`server is running on port: ${process.env.PORT}`)})
+})
+.catch((err)=>{
+    console.log("mongoDB connection failed!!!!!!!". err)
+}
+)
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+const asyncHandler = (reuestHandler) => {
+    (req, res, next) =>{
+        Promise.resolve(req, res, next)
+        .catch((err) =>{next (err)})
+    }
+}
+
+
+
+
+export {asyncHandler}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//function wrapper create /// try catch type
+//const asyncHandler = () => {} //create a async fx
+//const asyncHandler = (fn) => {async () => {}} // convert above in async handler
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//      try{
+
+//        await functin(req,res,next)
+
+//      }catch(error){
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message: error.message
+//         })
+//      }
+// }
